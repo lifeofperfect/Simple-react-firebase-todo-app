4e03848ad0c108ef940ac80a3c9834cd1b70c1e1
Igbadumhe <perfectimhetsike.igbadumhe@stanbicibtc.com>
2021-03-14 21:18:00 +0100

    added material ui to form and button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import './App.css';
 import { useState } from 'react';
+import {Button, FormControl, Input, InputLabel} from '@material-ui/core';
 
 function App() {
   const [todos, setTodos] = useState(["Take dog for a walk", "Take the rubbish out","Fire base is awesome", "react hook is new"])
+  const [input, setInput] = useState("");
+  console.log(input);
+
+  const addTodo=(e)=> {
+    e.preventDefault(); //stops referesh
+    console.log(input);
+    setTodos([...todos, input]) //set todos
+    setInput(""); //clear out the input stage
+    
+  }
 
   return (
     <div className="App">
       <h1>Hello worlds</h1>
-      <input />
-      <button>Add Todo</button>
+      <form>
+        
+        <FormControl>
+          <InputLabel>Write a Todo</InputLabel>
+          <Input value={input} onChange={event=> setInput(event.target.value)}/>
+        </FormControl>
+
+        <Button 
+          variant="contained" 
+          color="primary"
+          onClick={addTodo}
+          disabled={!input}
+          >
+          Add Todo
+        </Button>
+
+        {/* <button onClick={addTodo}>Add Todo</button> */}
+      </form>
 
       <ul>
         {todos.map(todo=> (
