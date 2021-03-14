7c62b70f5a85d31e201a7e74f93485c690c692e2
Igbadumhe <perfectimhetsike.igbadumhe@stanbicibtc.com>
2021-03-14 18:12:58 +0100

    install react and firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello worlds</h1>
+      <input />
+      <button>Add Todo</button>
+
+      <ul>
+        <li>Take dog for a walk</li>
+        <li>Take the rubbish out</li>
+        <li>Fire base is awesome</li>
+      </ul>
     </div>
   );
 }
