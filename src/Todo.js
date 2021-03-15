dab77e881c42a077a080b8785813d83bbda01503
Igbadumhe <perfectimhetsike.igbadumhe@stanbicibtc.com>
2021-03-15 01:53:45 +0100

    updated

--- src/Todo.js
+++ src/Todo.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import {Button, List, ListItem, ListItemAvatar,ListItemText} from '@material-ui/core';
+import React, {useState} from 'react'
+import {Button, List, ListItem, ListItemAvatar,ListItemText, Modal} from '@material-ui/core';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 import './Todo.css'
 import db from './firebase'
 
 function Todo(props) {
+    const [open, setOpen] = useState(false);
+
+    
     return (
+        
+        <>
+        
        <List className="todo__list">
            <ListItem>
                 <ListItemAvatar>
                 </ListItemAvatar>
                 <ListItemText primary={props.text.todo} secondary={props.text.todo} />
            </ListItem>
-           <Button onClick={event=> db.collection('todos').doc(props.text.id).delete()}>Delete me</Button>
+           <button onClick={e=> setOpen(true)}>Edit</button>
+           <DeleteForeverIcon onClick={event=> db.collection('todos').doc(props.text.id).delete()}></DeleteForeverIcon>
        </List>
+       </>
        
     )
 }
